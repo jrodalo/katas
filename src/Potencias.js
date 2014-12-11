bfec875133e7623a961279e77017334b8c10a1da
jrodalo <jrodalo@gmail.com>
2014-12-11 12:29:14 +0000

    Corregido un error con exponentes negativos

--- src/Potencias.js
+++ src/Potencias.js
@@ -6,6 +6,8 @@ var Potencias = {
 			return 1;
 		} else if (n === 1) {
 			return a;
+		} else if (n < 0) {
+			return 1 / Potencias.elevar(a, n * -1);
 		}
 
 		return a * a * Potencias.elevar(a, n - 2);
