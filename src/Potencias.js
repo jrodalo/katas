2cd394d0a1eae5eaf6422582a8942414a65630f6
jrodalo <jrodalo@gmail.com>
2014-12-11 12:16:42 +0000

    Corregido el método "elevar" para pasar los tests

--- src/Potencias.js
+++ src/Potencias.js
@@ -2,6 +2,12 @@ var Potencias = {
 
 	elevar: function(a, n) {
 
-		return 0;
+		if (n === 0) {
+			return 1;
+		} else if (n === 1) {
+			return a;
+		}
+
+		return a * a * Potencias.elevar(a, n - 2);
 	}
 }
